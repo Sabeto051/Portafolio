87b2a2ef6725b1bbc20c8f718504feac8e7c156b
Sabeto051 <bedoya.santi@gmail.com>
2020-03-11 02:55:56 -0500

    portafolio terminado

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ $(document).ready(function() {
     $('.custom-title').css({ 'background-position': traX + '%' + traY + '%' })
   })
 })
+
+$('.custom-txt').html(function(i, html) {
+  var chars = $.trim(html).split('')
+
+  return '<span>' + chars.join('</span><span>') + '</span>'
+})
